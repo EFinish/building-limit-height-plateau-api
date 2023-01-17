0c4ae0b97bbcc221cc16ccbadcea80328b8541eb
EFinish <EFinish@protonmail.com>
2023-01-17 17:38:29 +0100

    correct to longitude and lat

--- services/buildingdata-access/server.go
+++ services/buildingdata-access/server.go
@@ -7,11 +7,6 @@ import (
 	"google.golang.org/protobuf/types/known/structpb"
 )
 
-type Coordinate struct {
-	number1 float64
-	number2 float64
-}
-
 func (s *BuildingdataAccessServiceServer) CreateBuildingdata(ctx context.Context, req *protoOut.CreateBuildingdataRequest) (*protoOut.CreateBuildingdataResponse, error) {
 	// coordinates := req.BuildingLimits.Features[0].Geometry.Coordinates
 	ba.logger.Infof("potato")
@@ -19,10 +14,10 @@ func (s *BuildingdataAccessServiceServer) CreateBuildingdata(ctx context.Context
 	return &protoOut.CreateBuildingdataResponse{}, nil
 }
 
-func parseCoordinatesFromFeature(ctx context.Context, feature *protoOut.RequestFeatures) ([]Coordinate, error) {
+func parseCoordinatesFromFeature(ctx context.Context, feature *protoOut.RequestFeatures) ([]protoOut.Coordinate, error) {
 	coordinatesData := feature.Geometry.Coordinates
 
-	var foundCoordinates []Coordinate
+	var foundCoordinates []protoOut.Coordinate
 
 	if _, isFirstValueList := coordinatesData.Kind.(*structpb.Value_ListValue); isFirstValueList {
 		ba.logger.Debugf("first layer is a list")
@@ -31,8 +26,8 @@ func parseCoordinatesFromFeature(ctx context.Context, feature *protoOut.RequestF
 				ba.logger.Debugf("second layer is a list")
 				for _, thirdLayerValue := range secondLayerValue.GetListValue().Values {
 					if _, isThirdValueList := thirdLayerValue.Kind.(*structpb.Value_ListValue); isThirdValueList {
-						var number1 float64
-						var number2 float64
+						var latitude float64
+						var longitude float64
 
 						ba.logger.Debugf("third layer is a list")
 
@@ -44,16 +39,16 @@ func parseCoordinatesFromFeature(ctx context.Context, feature *protoOut.RequestF
 									// TODO throw warning
 								}
 
-								if number1 == 0 {
-									number1 = fourthLayerValue.GetNumberValue()
-								} else if number2 == 0 {
-									number2 = fourthLayerValue.GetNumberValue()
+								if longitude == 0 {
+									longitude = fourthLayerValue.GetNumberValue()
+								} else if latitude == 0 {
+									latitude = fourthLayerValue.GetNumberValue()
 								}
 							}
 						}
 
-						if number1 != 0 && number2 != 0 {
-							foundCoordinates = append(foundCoordinates, Coordinate{number1: number1, number2: number2})
+						if latitude != 0 && longitude != 0 {
+							foundCoordinates = append(foundCoordinates, protoOut.Coordinate{Latitude: latitude, Longitude: longitude})
 						}
 					}
 				}
